6f179fc2bec2f9f112b3fd638bb90cae7545a6bc
Sykkoss <damien.cosson@epitech.eu>
2020-03-30 14:03:04 +0200

    feat: Supply box reworked to be more generic + Valve suppliers done

---
+++ Assets/Scripts/Suppliers/Suppliers/SupplyBox.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupplyBox : ASupplier
+{
+    public GameObject _itemPrefab;
+
+
+    private void Start()
+    {
+        SetMaxCapacity(5);
+    }
+
+    public override void Tapped(Vector3 positionTapped)
+    {
+        SupplyItem(positionTapped);
+    }
+
+    public void SupplyItem(Vector3 position)
+    {
+        if (CurrentCapacity >= 1)
+            SpawnItem(position);
+        else
+            Refill();
+    }
+
+    private void SpawnItem(Vector3 position)
+    {
+        GameObject spawnedItem;
+        DraggableObject draggableItem;
+
+        spawnedItem = Instantiate(_itemPrefab, position, Quaternion.identity);
+        draggableItem = spawnedItem.GetComponent<DraggableObject>();
+        if (draggableItem != null)
+        {
+            draggableItem.SupplyBox = this;
+            StartCoroutine(draggableItem.Dragging());
+        }
+    }
+
+    protected void Refill()
+    {
+        ResetCurrentCapacity();
+    }
+}
