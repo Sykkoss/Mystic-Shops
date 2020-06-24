8e1a7470de07c6ca2319a024cfb0d66682a59d94
Sykkoss <damien.cosson@epitech.eu>
2020-06-24 16:46:23 +0200

    fix: OnGUI to update timer UI

--- Assets/Scripts/UI/InShop/Timer/DisplayTimer.cs
+++ Assets/Scripts/UI/InShop/Timer/DisplayTimer.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class DisplayTimer : MonoBehaviour
 {
+    [SerializeField]
     private Text _timerText;
 
     private void Start()
     {
-        _timerText = GetComponent<Text>();
         if (_timerText == null)
             Debug.LogError("Error: No component Text on gameObject named '" + name + "'.");
     }
 
-    private void Update()
+    private void OnGUI()
     {
         _timerText.text = PlayerTime.Instance.GetCurrentTime().ToString("0");
     }
