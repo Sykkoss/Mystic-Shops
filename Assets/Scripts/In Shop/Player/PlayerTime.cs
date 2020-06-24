d6da9faf38b6ac17ad7edf97e4ef1dcfbae9f95c
Sykkoss <damien.cosson@epitech.eu>
2020-06-24 16:43:24 +0200

    feat: Timer

---
+++ Assets/Scripts/In Shop/Player/PlayerTime.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTime : MonoBehaviour
+{
+    public static PlayerTime Instance;
+    public delegate void TimerFinishHandler();
+
+    public bool IsPaused { get; private set; }
+    public bool IsFinished { get; private set; }
+
+    public event TimerFinishHandler OnTimerFinish;
+
+    private float _currentTime;
+
+
+    private void Awake()
+    {
+        Instance = this;
+        IsPaused = true;
+        IsFinished = false;
+    }
+
+    private void Start()
+    {
+        InitTime(60f);
+        StartTimer();
+    }
+
+    private void Update()
+    {
+        DecreaseTime();
+    }
+
+    public void InitTime(float maxTime)
+    {
+        _currentTime = maxTime;
+    }
+
+    public void StartTimer()
+    {
+        IsPaused = false;
+    }
+
+    private void PauseTime()
+    {
+        IsPaused = !IsPaused;
+    }
+
+    private void DecreaseTime()
+    {
+        if (!IsPaused && !IsFinished)
+            _currentTime -= Time.deltaTime;
+        if (_currentTime <= 0f && !IsFinished)
+        {
+            _currentTime = 0f;
+            IsFinished = true;
+            OnTimerFinish?.Invoke();
+        }
+    }
+
+    public float GetCurrentTime() => _currentTime;
+}
---
+++ Assets/Scripts/UI/InShop/Timer/DisplayTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTimer : MonoBehaviour
+{
+    private Text _timerText;
+
+    private void Start()
+    {
+        _timerText = GetComponent<Text>();
+        if (_timerText == null)
+            Debug.LogError("Error: No component Text on gameObject named '" + name + "'.");
+    }
+
+    private void Update()
+    {
+        _timerText.text = PlayerTime.Instance.GetCurrentTime().ToString("0");
+    }
+}
